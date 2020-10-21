032934ad24012afbe6b6fcd0e8cf95d76a426a2c
jdaeth274 <joshdaeth@yahoo.co.uk>
2020-10-21 16:55:00 +0100

    sorting out missing isolates

---
+++ python/moving_missing_isolates_into_place.py
@@ -0,0 +1,46 @@
+import pandas
+import re
+
+with open("/rds/general/project/bacterial_evo_genomics/live/gps_annotations_4_2_2020/missing_refs/missing_fasta_list.txt") as myfile:
+	fasta_files = myfile.read.splitlines()
+
+with open("../missing_refs/missing_gff_list.txt") as file:
+	gff_files = file.read.splitlines()
+
+reference_loc = pandas.read_csv("/rds/general/project/bacterial_evo_genomics/live/gps_annotations_4_2_2020/gps_gubbins_runs/tot_missing_isolates.tsv", sep= "\t")
+references_tot = pandas.read_csv("./gps_run_data/gps_reference_isolate_fasta.csv")
+
+
+act_fasta_list = pandas.DataFrame()
+dna_list = []
+
+for k in range(len(reference_loc.index)):
+	current_isolate = reference_loc.iloc[k,0]
+	current_cluster = reference_loc.iloc[k,1]
+	current_pos_fa = "../missing_refs/" + current_isolate + ".contigs_velvet.fa"
+	new_pos_fa = current_isolate + "contigs_velvet.fasta"
+	new_pos_gff = current_isolate + "velvet.gff"
+	current_pos_gff = "../missing_refs/" + current_isolate + ".velvet.gff"
+	cluster_num = re.sub("gpsc\.","",current_cluster)
+	mv_fa = "mv " + current_pos_fa + " ../cluster_" + cluster_num + "_list_gffs/" + new_pos_fa
+	mv_gff = "mv " + current_pos_fa + " ../cluster_" + cluster_num + "_list_gffs/" + new_pos_gff
+	ref_fasta = references_tot[references_tot['cluster_name'] == current_cluster].iloc[0,1]
+	new_fasta_line = pandas.DataFrame()
+	new_fasta_loc = "/rds/general/project/bacterial_evo_genomics/live/gps_annotations_4_2_2020/cluster_" + cluster_num + "_list_gffs/" + new_pos_fa
+	new_fasta_line['fasta_loc'] = pandas.Series(new_fasta_loc)
+	new_fasta_line['refernce'] = pandas.Series(ref_fasta, index=new_fasta_line.index)
+	dna_list.append(new_fasta_loc)
+	act_fasta_list = act_fasta_list.append(new_fasta_line, ignore_index=True, sort=False)
+
+act_fasta_list.to_csv(path_or_buf="./missing_act_csv.csv", index=False)
+with open("./missing_fasta_list.txt", mode='wt', encoding='utf-8') as myfile:
+	myfile.write('\n'.join(dna_list))
+
+print("Finished")
+
+
+
+
+
+
+
