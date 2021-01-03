41281c7681fd74b36ed9de1fa352c686c214413f
jdaeth274 <joshdaeth@yahoo.co.uk>
2021-01-03 10:50:14 +0000

    tweaking python

--- python/pbp_blast_hits.py
+++ python/pbp_blast_hits.py
@@ -216,7 +216,7 @@ def outside_control(insertion_node, tree, example_id, act_comp_dir, ref_insertio
         skip = all_presence_checker(outside_isolates, nice_ids_tot)
 
     if skip == True:
-        return 0, False, False
+        return 0, False, False, False
 
 
 
@@ -233,7 +233,7 @@ def outside_control(insertion_node, tree, example_id, act_comp_dir, ref_insertio
     outside_iso = 0
     isolate_before_end = False
     isolate_after_start = False
-    strand = False
+    strand = "No"
     for ref in length_dict_sorted:
         if ref in nice_ids_tot and ref not in nice_ids_prev:
             continue
